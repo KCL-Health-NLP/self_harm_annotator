f64d21e9496ce6ee6f9615337e904878c979bd2b
Bittar <ABittar@slam.nhs.uk>
2020-10-14 23:49:29 +0100

    New branch with additional code for new heuristics -- testing as not giving same results

--- evaluate_patient_level.py
+++ evaluate_patient_level.py
@@ -2,6 +2,7 @@
 """
     This is a utility script to calculate agreement for Karyn Ayre's project.
 """
+import pandas as pd
 
 from ehost_annotation_reader import convert_file_annotations, get_corpus_files, load_mentions_with_attributes
 
@@ -10,6 +11,8 @@ __copyright__ = "Copyright 2020, André Bittar"
 __credits__ = ["André Bittar"]
 __license__ = "GPL"
 __email__ = "andre.bittar@kcl.ac.uk"
+
+HEURISTICS = ['base', '2m', '2m_diff', '2m_diff_strict']
 
 
 def has_DSH_mention(mentions):
@@ -25,7 +28,6 @@ def has_DSH_mention(mentions):
     Return:
         - bool: True if true mention is found, else False
     """
-    mentions = convert_file_annotations(mentions)
     for mention in mentions:
         polarity = mention.get('polarity', None)
         status = mention.get('status', None)
@@ -52,6 +54,7 @@ def get_brcid_mapping(ctype):
         - files: list; the list of files in the corpus
     """
     if ctype == 'gold':
+        """files = get_corpus_files('T:/Andre Bittar/Projects/KA_Self-harm/Restricted_Cohort_100_patients')"""
         files = get_corpus_files('T:/Andre Bittar/Projects/KA_Self-harm/Adjudication/train_dev')
     elif ctype == 'system':
         files = get_corpus_files('T:/Andre Bittar/Projects/KA_Self-harm/Adjudication/system_train_dev')
@@ -73,36 +76,146 @@ def get_brcid_mapping(ctype):
     return brcid_mapping, files
 
 
-def evaluate_patient_level(brcid_mapping, files):
+def check_prevalence(files, heuristic, cohort='full'):
+    """
+    Determine DSH prevalence by applying the heuristics to a list of annotated 
+    files.
+    """
+
+    if cohort not in ['full', 'restricted']:
+        raise ValueError('-- Invalid cohort:', cohort, 'choose "full" or "restricted"')
+
+    global_mentions = {}
+    xml = [x for x in files if 'xml' in x]
+    n = len(xml)
+
+    for i, f in enumerate(xml):
+        mentions = load_mentions_with_attributes(f)
+        mentions = convert_file_annotations(mentions)
+        brcid = f.split('\\')[1]
+        tmp = global_mentions.get(brcid, [])
+        tmp += mentions
+        global_mentions[brcid] = tmp
+
+        if i % 1000 == 0:
+            print(i, '/', n)
+
+    if cohort == 'restricted':
+        df = pd.read_excel('Z:/Karyn Ayre/Analysis/Restricted cohort/List_of_IDs_restricted_cohort.xlsx')
+        restr_brcids = df['brcid'].astype(str).tolist()
+        global_mentions = {brcid: global_mentions[brcid] for brcid in restr_brcids if brcid in global_mentions.keys()}
+
+    """Collect true mentions"""
+    global_true_mentions = {}
+    for brcid in global_mentions:
+        anns = global_mentions[brcid]
+        for ann in anns:
+            polarity = ann['polarity']
+            status = ann['status']
+            temporality = ann['temporality']
+            if polarity == 'POSITIVE' and status == 'RELEVANT' and temporality == 'CURRENT':
+                tmp = global_true_mentions.get(brcid, [])
+                tmp.append(ann['text'])
+                global_true_mentions[brcid] = tmp
+
+    """Apply heuristics"""
+    flagged = []
+    for brcid in global_true_mentions:
+        anns = global_true_mentions[brcid]
+        if heuristic == 'base':
+            if len(anns) > 0:
+                flagged.append(brcid)
+        elif heuristic == '2m':
+            if len(anns) > 1:
+                flagged.append(brcid)
+        elif heuristic == '2m_diff':
+            if len(anns) > 1 and len(set(anns)) > 1:
+                    flagged.append(brcid)
+        elif heuristic == '2m_diff_strict':
+            if len(anns) > 1 and len(anns) == len(set(anns)):
+                    flagged.append(brcid)
+    
+    print('Flagged patients:', len(flagged), '/', len(global_mentions), len(flagged) / len(global_mentions) * 100)
+    
+    return global_mentions
+
+
+def evaluate_patient_level(brcid_mapping, files, heuristic='2m', verbose=False):
     """
     Determine patient-level performance on the gold standard corpus.
     
     Arguments:
         - brcid_mapping: dict; a dictionary of BRCID mappings
         - files: list; a list of files in a corpus
+        - heuristic: str; the heuristic to apply
+              base: at least 1 true mention of DSH
+              2m_diff: at least 2 true mentions with different text
+              2m_diff_strict: at least 2 true mentions all with different text
     
     Return:
         - relevant_brcids: list; the list of BRCIDs for which a true DSH mention was found
     """
+
+    if heuristic not in HEURISTICS:
+        raise ValueError('-- incorrect heuristic', heuristic)
+
+    print('-- Using heuristic:', heuristic)
     
     # count relevant mentions in each file
     relevant_brcids = []
     for f in [x for x in files if 'xml' in x]:
         mentions = load_mentions_with_attributes(f)
-        if has_DSH_mention(mentions):
-            brcid = brcid_mapping.get(f.split('\\')[-1])
-            relevant_brcids.append(brcid)
-            print(f, brcid)
+        mentions = convert_file_annotations(mentions)
+        if heuristic == 'base':
+            if has_DSH_mention(mentions):
+                brcid = brcid_mapping.get(f.split('\\')[-1])
+                relevant_brcids.append(brcid)
+                if verbose:
+                    print(heuristic, f, brcid)
+        else:
+            true_text  = []
+            for mention in mentions:
+                polarity = mention.get('polarity', None)
+                status = mention.get('status', None)
+                temporality = mention.get('temporality', None)
+                if polarity == 'POSITIVE' and \
+                    status == 'RELEVANT' and \
+                    temporality == 'CURRENT':
+                        text = mention['text'] or ''
+                        text = text.strip()
+                        true_text.append(text)
+            text_unique = set(true_text)
+            if heuristic == '2m':
+                if len(true_text) > 1:
+                    brcid = brcid_mapping.get(f.split('\\')[-1])
+                    relevant_brcids.append(brcid)
+                    if verbose:
+                        print(heuristic, f, brcid)
+            if heuristic == '2m_diff':
+                if len(text_unique) > 1:
+                    brcid = brcid_mapping.get(f.split('\\')[-1])
+                    relevant_brcids.append(brcid)
+                    if verbose:
+                        print(heuristic, f, brcid)
+            elif heuristic == '2m_diff_strict':
+                if len(text_unique) > 1 and len(text_unique) == len(mentions):
+                    brcid = brcid_mapping.get(f.split('\\')[-1])
+                    relevant_brcids.append(brcid)
+                    if verbose:
+                        print(heuristic, f, brcid)
 
     return relevant_brcids
 
-
-if __name__ == '__main__':
+def process(heuristic='base'):
+    """
+    Run the whole evaluation process with a specific heuristic.
+    NB: gold must use the 'base' heuristic which is 1 true mention to flag a patient
+    """
     gold_brcid_mapping, gold_files = get_brcid_mapping('gold')
-    gold_brcids = set(evaluate_patient_level(gold_brcid_mapping, gold_files))
+    gold_brcids = set(evaluate_patient_level(gold_brcid_mapping, gold_files, heuristic='base', verbose=False))
     
     system_brcid_mapping, system_files = get_brcid_mapping('system')
-    system_brcids = set(evaluate_patient_level(system_brcid_mapping, system_files))
+    system_brcids = set(evaluate_patient_level(system_brcid_mapping, system_files, heuristic=heuristic, verbose=False))
     
     fn = len(gold_brcids.difference(system_brcids))
     fp = len(system_brcids.difference(gold_brcids))
